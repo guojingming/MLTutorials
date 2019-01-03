eb3893255b8502faeef87085e28b96741f6f350c
guojingming <1043840980@qq.com>
2019-01-03 18:08:58 +0800

    latest

--- demo.py
+++ demo.py
@@ -1,18 +1,23 @@
 from Regression import SingleVarLR
 
-trainingSet = (
-    (40, 50),
-    (44, 59),
-    (50, 61),
-    (60, 68),
-    (60, 64),
-    (70, 72),
-    (80, 83),
-    (90, 90),
-    (110, 93),
-    (106, 100)
-)
+# #Demo1: Single variable linear regression using BGD
+# def demo1():
+#     trainingSet = (
+#         (40, 50),
+#         (44, 59),
+#         (50, 61),
+#         (60, 68),
+#         (60, 64),
+#         (70, 72),
+#         (80, 83),
+#         (90, 90),
+#         (110, 93),
+#         (106, 100)
+#     )
+#     sv = SingleVarLR.Svlr(trainingSet)
+#     sv.train()
+#     print(sv.predict(180))
+#     print(sv.lastestResult)
 
-sv = SingleVarLR.Svlr(trainingSet)
-sv.train()
-print(sv.predict(180))
+# if __name__ == "__main__":
+#     demo1()
